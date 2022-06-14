02d7def1bda626f163cce2d500fd82ee0fd65f61
stefanyang <439354223@qq.com>
2022-06-14 12:41:58 +0800

    maven project init

---
+++ stefan/src/main/java/com/stefan/java/App.java
@@ -0,0 +1,16 @@
+package com.stefan.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "a");
+        map.put("2", "b");
+        map.put("3", "c");
+    }
+}
